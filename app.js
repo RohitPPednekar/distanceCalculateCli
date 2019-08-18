cadeafdfa0d0d8d056534a23b35131c4a1f85d47
Rohit Pednekar <rohitpednekar95@gmail.com>
2019-08-19 02:01:11 +0530

    Distance evaluated between bus location and first commuter location

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ standard_input.setEncoding('utf-8');
 var commutersAxis = [];
 var busLocationAxis = {x:0,y:0};
 var destinationLocationAxis = {x:0,y:0};
+var distance = 0.0;
 
 commuterLocationInput();
 function commuterLocationInput(){
@@ -94,14 +95,33 @@ function destinationLocationInput(){
 }
 
 function evaluateDistance(){
-    allignCommutersBusRouteWise();
+    
+    allignCommutersBusRouteWise().then((data)=>{
+        //Calculating first distance between bus location and first on the way coming commuter's location
+        distance+= getDistance(busLocationAxis.x, busLocationAxis.y, data[0].x, data[0].y);
+        console.log("distance-->"+distance)
+    });
 }
 
 function allignCommutersBusRouteWise(){
-
-//squared distance
-const sqDist = (pointa, pointb) => (pointa.x-pointb.x)**2+(pointa.y-pointb.y)**2;
-//sorting
-var sortedCommuterAxes = commutersAxis.sort((pointa, pointb) => sqDist(busLocationAxis,pointa)-sqDist(busLocationAxis,pointb));
- console.log("allignCommutersBusRouteWise--->"+JSON.stringify(sortedCommuterAxes))
+    return new Promise((resolve,reject)=>{
+        //squared distance
+        const sqDist = (pointa, pointb) => (pointa.x-pointb.x)**2+(pointa.y-pointb.y)**2;
+        //sorting of commuters location as per bus location
+        var sortedCommuterAxes = commutersAxis.sort((pointa, pointb) => sqDist(busLocationAxis,pointa)-sqDist(busLocationAxis,pointb));
+        resolve(sortedCommuterAxes);
+        console.log("allignCommutersBusRouteWise--->"+JSON.stringify(sortedCommuterAxes))
+    });
 }
+
+
+//Calculating distance between two axis
+function getDistance( x1, y1, x2, y2 ) {
+	
+	var xs = x2 - x1,
+		ys = y2 - y1;		
+	
+	xs *= xs;
+	ys *= ys;
+	return Math.sqrt( xs + ys );
+};
